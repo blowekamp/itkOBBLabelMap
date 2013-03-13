45ffec0fdb56916de5b152f0c743fe66d15e4be6
Bradley Lowekamp <blowekamp@mail.nih.gov>
2013-03-13 11:12:36 -0400

    Renaming module in cmake configs

--- itk-module.cmake
+++ itk-module.cmake
@@ -4,13 +4,13 @@
 get_filename_component( MY_CURENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 file( READ "${MY_CURENT_DIR}/README" DOCUMENTATION )
 
-# itk_module() defines the module dependencies in ITKExternalTemplate
-# ITKExternalTemplate depends on ITKCommon
-# The testing module in ITKExternalTemplate depends on ITKTestKernel
-# and ITKMetaIO(besides ITKExternalTemplate and ITKCore)
+# itk_module() defines the module dependencies in ITKOBBLabelMap
+# ITKOBBLabelMap depends on ITKCommon
+# The testing module in ITKOBBLabelMap depends on ITKTestKernel
+# and ITKMetaIO(besides ITKOBBLabelMap and ITKCore)
  
 # define the dependencies of the include module and the tests
-itk_module(ITKExternalTemplate
+itk_module(ITKOBBLabelMap
   DEPENDS
     ITKCommon
   TEST_DEPENDS
