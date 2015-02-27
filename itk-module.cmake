715ed818900af579be862f81123c6b4c62f907d0
Bradley Lowekamp <blowekamp@mail.nih.gov>
2015-02-27 09:12:49 -0500

    Exclude module from being turned on by default

--- itk-module.cmake
+++ itk-module.cmake
@@ -4,6 +4,12 @@
 get_filename_component( MY_CURENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 file( READ "${MY_CURENT_DIR}/README" DOCUMENTATION )
 
+# ITK version 4.5 changed it from EXCLUDE_FROM_ALL to EXCLUDE_FROM_DEFAULT
+set( _EXCLUDE "EXCLUDE_FROM_ALL" )
+if (NOT "${ITK_VERSION_MAJOR}.${ITK_VERSION_MINOR}.${ITK_VERSION_MINOR_PATCH}" VERSION_LESS "4.5")
+  set( _EXCLUDE "EXCLUDE_FROM_DEFAULT" )
+endif()
+
 # itk_module() defines the module dependencies in ITKOBBLabelMap
 # ITKOBBLabelMap depends on ITKCommon
 # The testing module in ITKOBBLabelMap depends on ITKTestKernel
@@ -19,4 +25,5 @@ itk_module(ITKOBBLabelMap
     ITKMetaIO
   DESCRIPTION
     "${DOCUMENTATION}"
+  ${_EXCLUDE}
 )
